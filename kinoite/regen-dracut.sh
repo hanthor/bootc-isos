90f38fa6fa1a54aaa730eb2dca742ec734b6e6a9
Ondřej Budai <ondrej@budai.cz>
2026-01-13 16:32:30 +0000

    kinoite: align with bazzite

--- kinoite/regen-dracut.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-INSTALLED_KERNEL=$(kernel-install list --json short | jq -r 'map(select(.has_kernel == true)) | .[0].version')
-DRACUT_NO_XATTR=1 dracut -v --force --zstd --reproducible --no-hostonly --add "dmsquash-live dmsquash-live-autooverlay" "/usr/lib/modules/${INSTALLED_KERNEL}/initramfs.img" "${INSTALLED_KERNEL}"
